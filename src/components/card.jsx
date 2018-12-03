e48cb7ba529604327e0e06c78d9927628bb82744
Akash Agarwal <akashaggarwal777@gmail.com>
2018-12-03 16:37:37 +1100

    cardComponent: Variable related changes Rename variable descriptionShort to shortDesc Add check for progress Make progress child of data

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,14 +7,14 @@ class Card extends Component {
         <div className={"card-panel card_" + this.props.id}>
           <div className="card-content white-text">
             <span className="card-title">{this.props.data.title}</span>
-            <p>{this.props.data.descriptionShort}</p>
+            <p>{this.props.data.shortDesc}</p>
           </div>
           <div className="row" style={{marginBottom:"0px"}}>
             <p className="col left">Lesson Progress</p>
-            <p className="col right">{this.props.progress}%</p>
+            <p className="col right">{this.props.data.progress === ''? 0 : this.props.data.progress}%</p>
           </div>
           <div className="progress">
-            <div className="determinate" style={{width:this.props.progress+"%"}}></div>
+            <div className="determinate" style={{width: (this.props.data.progress === ''? 0 : this.props.data.progress) +"%"}}></div>
           </div>
           <div className="card-action">
             <a className="flat-btn" href="#!">Visit</a>
