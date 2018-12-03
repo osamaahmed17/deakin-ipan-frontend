9fa678f8094669bc5665cdcbf0f76634bf2beef1
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-03 17:46:00 +1100

    collapsibleComponent: New collapsible

---
+++ src/components/collapsible.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import M from "materialize-css";
+
+class Collapsible extends Component {
+  componentDidMount() {
+    const options = {
+      accordion: false,
+    };
+    M.Collapsible.init(this.Collapsible, options);
+  }
+  render () {
+    return(
+      <div className="container">
+        <ul className="collapsible expandable" ref={Collapsible => { this.Collapsible = Collapsible; }}>
+          {
+            this.props.data.map((item, i) => {
+              return (
+                <li key = {i}>
+                  <div className="collapsible-header"> {item.title} </div>
+                  <div className="collapsible-body"> <span> {item.desc} </span> </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Collapsible;
