c4ac1ec50eb2ae5fab48cbf181f17b5c177f3abc
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-03 15:06:24 +1100

    cardComponent: Fix card size on screen change

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Card = () => {
-  console.log("Card Component")
-  return (
-    <div className="col s12 m6">
-      <div className={"card blue-grey darken-1 card_" + this.props.id}>
-        <div className="card-content white-text">
-          <span className="card-title">{this.props.data.title}</span>
-          <p>{this.props.data.descriptionShort}</p>
-        </div>
-        <div className="card-action">
-          <a href="#!">Visit</a>
+class Card extends Component {
+  render() {
+    return (
+      <div className="col s12 m12 l6 offset-l3">
+        <div className={"card blue-grey darken-1 card_" + this.props.id}>
+          <div className="card-content white-text">
+            <span className="card-title">{this.props.data.title}</span>
+            <p>{this.props.data.descriptionShort}</p>
+          </div>
+          <div className="card-action">
+            <a href="#!">Visit</a>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } 
 }
 
 export default Card;
