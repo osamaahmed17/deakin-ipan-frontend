a53668f469ad33a1cae827fad3f534d9dff217c6
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-03 16:27:07 +1100

    cardComponent: Add progress bar

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,13 +4,20 @@ class Card extends Component {
   render() {
     return (
       <div className="col s12 m12 l6 offset-l3">
-        <div className={"card blue-grey darken-1 card_" + this.props.id}>
+        <div className={"card-panel card_" + this.props.id}>
           <div className="card-content white-text">
             <span className="card-title">{this.props.data.title}</span>
             <p>{this.props.data.descriptionShort}</p>
           </div>
+          <div className="row" style={{marginBottom:"0px"}}>
+            <p className="col left">Lesson Progress</p>
+            <p className="col right">{this.props.progress}%</p>
+          </div>
+          <div className="progress">
+            <div className="determinate" style={{width:this.props.progress+"%"}}></div>
+          </div>
           <div className="card-action">
-            <a href="#!">Visit</a>
+            <a className="flat-btn" href="#!">Visit</a>
           </div>
         </div>
       </div>
