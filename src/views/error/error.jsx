bf54986abbb74fe890642bd0e929aedd493508c2
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-04 14:45:45 +1100

    errorView: New Error view

---
+++ src/views/error/error.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+  renderMessage = (errorCode) => {
+    let msg = '';
+    switch (errorCode) {
+      case 404:
+        msg = 'Page not found';
+        break;
+      case 401:
+        msg = 'Not authorised';
+        break;
+    }
+    return msg;
+  }
+
+  render() {
+    return (
+      <div className="errorStyle">
+        {/* <i className="large material-icons">error_outline</i> */}
+        <h1>{this.props.errorCode}</h1>
+        <p>{this.renderMessage(this.props.errorCode)}</p>
+      </div>
+    )
+  }
+}
+
+export default Error;
