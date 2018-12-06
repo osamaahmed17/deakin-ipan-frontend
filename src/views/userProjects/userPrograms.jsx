d2a9b266616291e97d87c87a1ad854a858a281ce
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-06 13:23:00 +1100

    userPrograms: New condition to check programs

--- src/views/userProjects/userPrograms.jsx
+++ src/views/userProjects/userPrograms.jsx
@@ -24,30 +24,47 @@ class UserPrograms extends Component {
     API.getPrograms(this.stateHandler);
   }
 
+  checkPrograms = (data) => {
+    if (data.length > 0) {
+      return (
+        <div>
+          <p>Your Programs</p>
+            {
+              data.map((items) => {
+                return (
+                  <div key={items.id}>
+                    <Link to={'/programs/' + items.id}>
+                      <Card data={items} p_id={items.id}>
+                        <div className="row" style={{marginBottom:"0px"}}>
+                          <p className="col right">Completed Modules {items.progress === ''? 0 : items.progress}/{items.total}</p>
+                        </div>
+                        <div className="progress">
+                          <div className="determinate" style={{width: (items.progress === ''? 0 : items.progress) +"%"}}></div>
+                        </div>
+                      </Card>
+                    </Link>
+                  </div>
+                )
+              })
+            } 
+        </div>
+      )
+    } else {
+      return (
+        <div className="row padding-top">
+          <i className="material-icons md-light large">add_circle_outline</i>
+          <p>Add New Program</p>
+        </div>
+      )
+    }
+  }
+
   render() {
     let programs = this.state.programs;
     if (!this.state.programs) return <LoadingComponent />;
     return (
       <div className="container">
-        <p>Your Programs</p>
-        {
-          programs.map((items) => {
-            return (
-              <div key={items.id}>
-                <Link to={'/programs/' + items.id}>
-                  <Card data={items} p_id={items.id}>
-                    <div className="row" style={{marginBottom:"0px"}}>
-                      <p className="col right">Completed Modules {items.progress === ''? 0 : items.progress}/{items.total}</p>
-                    </div>
-                    <div className="progress">
-                      <div className="determinate" style={{width: (items.progress === ''? 0 : items.progress) +"%"}}></div>
-                    </div>
-                  </Card>
-                </Link>
-              </div>
-            )
-          })
-        }
+        {this.checkPrograms(programs)}
       </div>
     )
   }
