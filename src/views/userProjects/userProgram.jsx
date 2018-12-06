4796eff25b132a7dbbfd4c77917ddeacb3295d7e
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-06 14:10:29 +1100

    userProgram: Fix card children

--- src/views/userProjects/userProgram.jsx
+++ src/views/userProjects/userProgram.jsx
@@ -8,7 +8,7 @@ class UserProgram extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      program: null
+      program: null,
     } 
   }
 
@@ -24,18 +24,25 @@ class UserProgram extends Component {
     API.getProgram(this.stateHandler, this.props.match.params.p_id);
   }
 
+
   render () {
     let program = this.state.program;
     if (!this.state.program) return <LoadingComponent />;
+    // this.countOccurence("/","/programs/1/modules/1/activities/1")
     return(
       <div className="container">
         <p>Program {this.props.match.params.p_id}</p>
         {
-          program.modules.map((items) => {
+          program.modules.map((items, id) => {
             return (
-              <div key={program.id}>
+              <div key={id}>
                 <Link to={'/programs/' + this.props.match.params.p_id + '/modules/' + program.id}>
-                  <Card data={program} p_id={this.props.match.params.id} m_id = {program.id} />
+                {/* the status in card is for achivements and favourites */}
+                  <Card data={items} p_id={this.props.match.params.id} m_id = {program.id} status={true}>
+                    <div className="row margin-bot-rm">
+                      <p className="col right" style={{marginBottom:"0px"}}>Status: {program.status}</p>
+                      </div>
+                  </Card>
                 </Link>
               </div>
             )
