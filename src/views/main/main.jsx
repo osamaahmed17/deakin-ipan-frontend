66569a7db322da2a340be4a66f1f1246c30151bd
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-07 14:15:59 +1100

    mainViews: Import new Calendar

--- src/views/main/main.jsx
+++ src/views/main/main.jsx
@@ -7,7 +7,7 @@ import UserProgram from 'views/userProjects/userProgram.jsx'
 import UserModule from 'views/userProjects/userModule.jsx'
 import UserActivity from 'views/userProjects/userActivity.jsx'
 import UserTask from 'views/userProjects/userTasks.jsx'
-import Calender from 'views/calender/calender.jsx'
+import Calendar from 'react-calendar';
 import Profile from 'views/profile/profile.jsx'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import AppHelper from 'helpers/AppHelper'
@@ -30,7 +30,7 @@ class Main extends Component {
             <Route exact path='/programs/:p_id/modules/:m_id/tasks/:t_id' component={UserTask} />
             <Route exact path='/activity/:a_id' component={UserActivity} />
             <Route exaxt path='/tasks/:t_id' component={UserTask} />
-            <Route exact path='/calender' component={Calender} />
+            <Route exact path='/calender' component={Calendar} />
             <Route render= {(props) => (
               <Error {...props} errorCode={401} />
             )} />
