04a064b8b9584f1eb53758fdf5f51ce06803f43c
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-07 12:06:52 +1100

    Update css class name

--- src/views/userProjects/userProgram.jsx
+++ src/views/userProjects/userProgram.jsx
@@ -38,7 +38,7 @@ class UserProgram extends Component {
                 {/* the status in card is for achivements and favourites */}
                   <Card data={items} p_id={this.props.match.params.id} m_id = {program.id} status={true}>
                     <Link to={'/programs/' + this.props.match.params.p_id + '/modules/' + program.id}>
-                      <p className="justify-content text-color">{items.shortDescription}</p>
+                      <p className="justify-content text-color-white">{items.shortDescription}</p>
                     </Link>
                     <div className="row margin-bot-rm">
                       <p className="col right" style={{marginBottom:"0px"}}>Status: {program.status}</p>
