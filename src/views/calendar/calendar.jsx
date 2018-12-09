de39f18ab72f05c6495e05cc614ed4decb87c64f
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-09 20:15:42 +1100

    Cleanup

--- src/views/calendar/calendar.jsx
+++ src/views/calendar/calendar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
-import { type } from 'os';
 
 class ViewCalender extends Component {
   constructor(props) {
