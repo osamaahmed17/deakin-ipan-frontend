4408ad91c41502a7a3ef6ccaf7eaada7e3d72f43
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-12 17:29:33 +1100

    New View

---
+++ src/views/userProjects/favouriteActivities.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import API from 'helpers/api.js'
+import LoadingComponent from 'components/loading/loading'
+import Card from 'components/cards/card.jsx'
+
+class FavouriteActivity extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favouriteActivities: null,
+    }
+  }
+
+  componentDidMount() {
+    this.favouriteActivities();
+  }
+
+  stateHandler = (state) => {
+    this.setState(state);
+  }
+
+  favouriteActivities = () => {
+  API.getFavouriteActivities(this.stateHandler);
+  }
+
+  render() {
+    let favouriteActivities = this.state.favouriteActivities;
+    if (!this.state.favouriteActivities) return <LoadingComponent />;
+    return (
+      <div className="container">
+        <p className="heading center">My Favourite Activities</p>
+        {
+          favouriteActivities.map((data, i) => {
+            return (
+              <Card data={data}>
+                <div className="row">
+                  <div className="col s9 left-align"> {data.shortDescription} </div>
+                  <div className="col s3"> {data.status} </div>
+                </div>
+              </Card>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default FavouriteActivity;
---
+++ src/views/userProjects/favouriteModules.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import API from 'helpers/api.js'
+import ModuleCard from 'components/cards/moduleCard.jsx'
+import LoadingComponent from 'components/loading/loading'
+
+class FavouriteModule extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favouriteModules: null,
+    }
+  }
+
+  componentDidMount() {
+    this.getFavouriteModules();
+  }
+
+  stateHandler = (state) => {
+    this.setState(state);
+  }
+
+  getFavouriteModules = () => {
+    API.getFavouriteModules(this.stateHandler);
+  }
+
+  mapCard = (data) => {
+    if(data.favourite.status === "true") {
+      return (
+        <ModuleCard data={data}>
+          <p className="justify-content text-color-white description">{data.shortDescription}</p>
+          <div className="row margin-bot-rm">
+            <p className="col right" style={{marginBottom:"0px"}}> {data.status} </p>
+          </div>
+        </ModuleCard>
+      )
+    }
+  }
+
+  render() {
+    let favouriteModules = this.state.favouriteModules;
+    if (!this.state.favouriteModules) return <LoadingComponent />;
+    return (
+      <div className="container">
+        <p className="heading center">My Favourite Modules</p>
+        {
+          favouriteModules.map((array, i) => {
+            return (
+              <div key={i}>
+                {
+                  array.modules.map((item, j) => {
+                    return (
+                      <div key={j}> {this.mapCard(item)} </div>
+                    )
+                  })
+                }
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default FavouriteModule;
