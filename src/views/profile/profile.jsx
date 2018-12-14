09dd5f443b0f9373eab11817b50bbe255389a691
Nirav Bhimani <niravbhimani53@gmail.com>
2018-12-14 14:58:45 +1100

    profileView: Make profile pretty

--- src/views/profile/profile.jsx
+++ src/views/profile/profile.jsx
@@ -27,26 +27,35 @@ class Profile extends Component {
     let profile = this.state.profile;
     if (!this.state.profile) return <LoadingComponent/>;
     return (
-      <div>
-        <h1>My Profile</h1>
-        <p>
-          First Name: {profile.first_name}
-        </p>
-        <p>
-          Last Name: {profile.last_name}
-        </p>
-        <p>
-          DOB: {profile.dob}
-        </p>
-        <p>
-          Mobile: {profile.mobile}
-        </p>
-        <p>
-          Email: {profile.email}
-        </p>
-        <p>
-          Gender: {profile.gender}
-        </p>
+      <div className="profile">
+        <div className="container row">
+          <h2 className="left-align">My Profile</h2>
+        </div>
+        <img src="https://imgur.com/9EHx6W8.png" alt="Avatar" className="circle responsive-img"/>
+        <div className="container row">
+          <div className="col s5 left-align"> First Name: </div>
+          <div className="col s7 left-align"> {profile.first_name} </div>
+        </div>
+        <div className="container row">
+          <div className="col s5 left-align"> Last Name: </div>
+          <div className="col s7 left-align"> {profile.last_name} </div>
+        </div>
+        <div className="container row">
+          <div className="col s5 left-align"> Date of Birth: </div>
+          <div className="col s7 left-align"> {profile.dob} </div>
+        </div>
+        <div className="container row">
+          <div className="col s5 left-align"> Mobile: </div>
+          <div className="col s7 left-align"> {profile.mobile} </div>
+        </div>
+        <div className="container row">
+          <div className="col s5 left-align"> Email: </div>
+          <div className="col s7 left-align"> {profile.email} </div>
+        </div>
+        <div className="container row">
+          <div className="col s5 left-align"> Gender: </div>
+          <div className="col s7 left-align"> {profile.gender} </div>
+        </div>
       </div>
     );
   }
