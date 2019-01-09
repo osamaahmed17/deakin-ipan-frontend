0517a97b14f73bfdb5a52d2f32d01c65afc95e0a
Nirav Bhimani <niravbhimani53@gmail.com>
2019-01-09 15:26:03 +1100

    taskSummaryView: User cant visit summary page before attempting quiz

--- src/views/taskSummary/taskSummary.jsx
+++ src/views/taskSummary/taskSummary.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { CONSTANTS } from 'helpers/urlConstants.js'
+var _ = require('underscore');
 
 class TaskSummary extends Component{
   displayResult = (data,  key) => {
@@ -26,18 +27,18 @@ class TaskSummary extends Component{
   }
 
   render() {
-    if(this.props.location.state.quizRecord.length === 0) return (
+    // http://localhost:3000/programs/1/modules/1/tasks/1/summary
+    if(!_.isEqual(this.props.location, undefined)) return (
       <div className="TaskSummary container">
         <div className="main-title left-align">
           Task Summary
         </div>
         <div className="summary container left-align">
-          <p className="task-attempt-status">Total Questions attempted {this.props.location.state.quizRecord.length} out of {this.props.location.state.totalNumberOfQuestions}.</p>
-          <p> Please attempt quiz again.</p>
+          <p> Please finish the task {this.props.match.params.t_id} first.</p>
         </div>
         <div className="container">
           <Link to={{ pathname: CONSTANTS.PROGRAMS}}>
-            <button className="waves-effect waves-light btn back-to-module-btn" id="back-to-module-btn"> Back to Module</button>
+            <button className="waves-effect waves-light btn back-to-module-btn" id="back-to-program-btn"> Back to Programs</button>
           </Link>
         </div>
       </div>
@@ -57,7 +58,7 @@ class TaskSummary extends Component{
         </div>
         <div className="container">
           <Link to={{ pathname: CONSTANTS.PROGRAMS}}>
-            <button className="waves-effect waves-light btn back-to-module-btn" id="back-to-module-btn"> Back to Module</button>
+            <button className="waves-effect waves-light btn back-to-program-btn" id="back-to-module-btn"> Back to Programs</button>
           </Link>
         </div>
       </div>
