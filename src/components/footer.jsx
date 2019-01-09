28ce3e0cd76a1cab747be8fad42b2886df0d6eca
Nirav Bhimani <niravbhimani53@gmail.com>
2019-01-09 15:31:15 +1100

    footerComponent: Wrap app name in link tag

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { CONSTANTS } from 'helpers/urlConstants.js'
 
 class Footer extends Component {
   render() {
     return (
       <footer className="Footer page-footer footer-fixed">
         <div className="container">
-          <span className="app-name center" id="app-name"> IPAN </span>
+          <Link to={{pathname: CONSTANTS.PROGRAMS}}>
+            <span className="app-name center" id="app-name"> IPAN </span>
+          </Link>
         </div>
       </footer>
     )
