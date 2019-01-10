00e5b72d25142e3ab8fc819d5c46ff56fc28a2bf
Akash Agarwal <akashaggarwal777@gmail.com>
2019-01-11 09:35:07 +1100

    socketHandler.js: Add notification event listener

--- src/helpers/socketHander.js
+++ src/helpers/socketHander.js
@@ -9,6 +9,10 @@ const initializeSocketListener = function () {
   console.log('Socket initialized')
   socket = io(process.env.REACT_APP_BASE_URL)
   socket.on('connect', onConnection)
+  socket.on('notification', (data) => {
+    console.log('notification recieved', data.text)
+    let element = document.getElementById('notification-dropdown');
+  })
 }
 
 export default initializeSocketListener
