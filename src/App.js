699df2c80e16ad957cb6e60f56df8baf833a3793
Nirav Bhimani <niravbhimani53@gmail.com>
2019-01-11 14:21:49 +1100

    appJS: Pass history as props in footer Auto format document

--- src/App.js
+++ src/App.js
@@ -63,52 +63,52 @@ class App extends Component {
     else return (
       <div className="App">
         {/* Header */}
-        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Header history= {this.props.history} location={this.props.location} title={this.state.title} logout={this.stateHandler} /> : ''}
+        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Header history={this.props.history} location={this.props.location} title={this.state.title} logout={this.stateHandler} /> : ''}
 
         {/* Main body */}
         <Switch>
 
           <Route exact path='/' render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
-            <Redirect to='/programs' /> : <Login parentState={this.state} parentProps={this.props} /> )} />
+            <Redirect to='/programs' /> : <Login parentState={this.state} parentProps={this.props} />)} />
 
-          <Route exact path='/home' render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <Home {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path='/home' render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <Home {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.PROGRAMS)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <UserPrograms {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.PROGRAMS)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <UserPrograms {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.PROGRAM, [":p_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <UserProgram {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.PROGRAM, [":p_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <UserProgram {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.MODULES, [":p_id", ":m_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <UserModule {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.MODULES, [":p_id", ":m_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <UserModule {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.ACTIVITIES, [":p_id", ":m_id", ":a_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <UserActivity {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.ACTIVITIES, [":p_id", ":m_id", ":a_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <UserActivity {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.TASKS, [":p_id", ":m_id", ":t_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <UserTask {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.TASKS, [":p_id", ":m_id", ":t_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <UserTask {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {CONSTANTS.CALENDAR} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <ViewCalendar {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={CONSTANTS.CALENDAR} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <ViewCalendar {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.RESOURCES)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <Resources {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.RESOURCES)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <Resources {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.FAVOURITE_ACTIVITIES)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <FavouriteActivities {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.FAVOURITE_ACTIVITIES)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <FavouriteActivities {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.FAVOURITE_MODULES)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <FavouriteModules {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.FAVOURITE_MODULES)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <FavouriteModules {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.TASK_SUMMARY, [":p_id", ":m_id", ":t_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <TaskSummary {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.TASK_SUMMARY, [":p_id", ":m_id", ":t_id"])} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <TaskSummary {...props} /> : <Redirect to='/' />)} />
 
-          <Route exact path= {replacePlaceHolder(CONSTANTS.NOTIFICATIONS)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-            <Notifications {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.NOTIFICATIONS)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <Notifications {...props} /> : <Redirect to='/' />)} />
 
-            <Route exact path= {replacePlaceHolder(CONSTANTS.PROFILE)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? 
-              <Profile {...props}/> : <Redirect to='/' /> )} />
+          <Route exact path={replacePlaceHolder(CONSTANTS.PROFILE)} render={(props) => (this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ?
+            <Profile {...props} /> : <Redirect to='/' />)} />
 
           <Route exact path='/test' render={() => <div>Test</div>} />
 
@@ -117,7 +117,7 @@ class App extends Component {
         </Switch>
 
         {/* Footer */}
-        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Footer /> : ''}
+        {this.props.loggedIn || AppHelper.isUserLocalStorageLoggedIn() ? <Footer history={this.props.history} location={this.props.location} /> : ''}
       </div>
     );
   }
