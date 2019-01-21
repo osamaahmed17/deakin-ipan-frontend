97ca1437aa859789c7d25a3d09637e063ea20527
Nirav Bhimani <niravbhimani53@gmail.com>
2019-01-21 13:36:05 +1100

    profileView: Profile details rednered inside card Profile Image aligned left

--- src/views/profile/profile.jsx
+++ src/views/profile/profile.jsx
@@ -8,7 +8,7 @@ class Profile extends Component {
     this.state = {
       profile: null
     }
-    
+
   }
 
   componentDidMount() {
@@ -25,36 +25,42 @@ class Profile extends Component {
 
   render() {
     let profile = this.state.profile;
-    if (!this.state.profile) return <LoadingComponent/>;
+    if (!this.state.profile) return <LoadingComponent />;
     return (
-      <div className="Profile">
-        <div className="container row">
-          <h2 className="main-title left-align">My Profile</h2>
-        </div>
-        <img src="https://imgur.com/9EHx6W8.png" alt="Avatar" className="user-img circle responsive-img"/>
-        <div className="container row">
-          <div className="col s4 left-align"> First Name: </div>
-          <div className="col s8 left-align first-name"> {profile.firstName} </div>
-        </div>
-        <div className="container row">
-          <div className="col s4 left-align"> Last Name: </div>
-          <div className="col s8 left-align last-name"> {profile.lastName} </div>
-        </div>
-        <div className="container row">
-          <div className="col s4 left-align"> Date of Birth: </div>
-          <div className="col s8 left-align dob"> {profile.dob} </div>
-        </div>
-        <div className="container row">
-          <div className="col s4 left-align"> Mobile: </div>
-          <div className="col s8 left-align mobile"> {profile.mobile} </div>
-        </div>
-        <div className="container row">
-          <div className="col s4 left-align"> Email: </div>
-          <div className="col s8 left-align email"> {profile.email} </div>
-        </div>
-        <div className="container row">
-          <div className="col s4 left-align"> Gender: </div>
-          <div className="col s8 left-align gender"> {profile.gender} </div>
+      <div className="Profile container">
+        <p className="main-title left-align">My Profile</p>
+        <div className="col s12 m6 l6 center-align">
+          <div className="card">
+            <div className="card-content">
+              <div className="user-profile-image left-align">
+                <img src="https://imgur.com/9EHx6W8.png" alt="Avatar" className="user-img circle responsive-img" />
+              </div>
+              <div className="row">
+                <div className="col s5 left-align label"> First Name: </div>
+                <div className="col s7 left-align first-name"> {profile.firstName} </div>
+              </div>
+              <div className="row">
+                <div className="col s5 left-align label"> Last Name: </div>
+                <div className="col s7 left-align last-name"> {profile.lastName} </div>
+              </div>
+              <div className="row">
+                <div className="col s5 left-align label"> Date of Birth: </div>
+                <div className="col s7 left-align dob"> {profile.dob} </div>
+              </div>
+              <div className="row">
+                <div className="col s5 left-align label"> Mobile: </div>
+                <div className="col s7 left-align mobile"> {profile.mobile} </div>
+              </div>
+              <div className="row">
+                <div className="col s5 left-align label"> Email: </div>
+                <div className="col s7 left-align email"> {profile.email} </div>
+              </div>
+              <div className="row">
+                <div className="col s5 left-align label"> Gender: </div>
+                <div className="col s7 left-align gender"> {profile.gender} </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
