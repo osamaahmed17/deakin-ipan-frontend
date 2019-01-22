01d99d994f69f8def5b30e49f9f3da86d97f1827
Nirav Bhimani <niravbhimani53@gmail.com>
2019-01-22 21:47:28 +1100

    Favourite icon click trigger api to toggle favourite status on backend

--- src/components/cards/favouriteActivitiesCard.jsx
+++ src/components/cards/favouriteActivitiesCard.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { CONSTANTS } from 'helpers/urlConstants.js'
 import { replacePlaceHolder } from 'helpers/urlHelper.js'
+import API from 'helpers/api.js'
 
 class FavouriteActivitiesCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggleFavourite: true,
+      favouriteStatus: true,
     }
-    this.handleFavouriteClick = this.handleFavouriteClick.bind(this)
   }
 
   completionStatus(status) {
@@ -20,10 +20,12 @@ class FavouriteActivitiesCard extends Component {
     }
   }
 
-  handleFavouriteClick() {
-    this.setState(state => ({
-      toggleFavourite: !state.toggleFavourite
-    }))
+  handleFavouriteClick = () => {
+    API.toggleFavouriteActivity(this.props.data.program.id, this.props.data.module.id, this.props.data.activity.id, this.stateHandler, this.state.favouriteStatus)
+  }
+
+  stateHandler = (state) => {
+    this.setState(state);
   }
 
   render() {
@@ -33,7 +35,7 @@ class FavouriteActivitiesCard extends Component {
           <div className="card-content">
             <div className="row">
               <div className="col s11 m11 l11 card-title left-align"> Module {this.props.data.module.id}: {this.props.data.module.title} </div>
-              <i className="col s1 m1 l1 material-icons btn-flat" style={{opacity: this.state.toggleFavourite ? '1.0' : '0.2'}} onClick={this.handleFavouriteClick}> favorite </i>
+              <i className="col s1 m1 l1 material-icons btn-flat favourite-icon" style={{ opacity: this.state.favouriteStatus ? '1.0' : '0.2'}} onClick={this.handleFavouriteClick}> favorite </i>
             </div>
           </div>
           <Link to={replacePlaceHolder(CONSTANTS.ACTIVITIES,[this.props.data.program.id, this.props.data.module.id, this.props.data.activity.id])}>
--- src/components/cards/favouriteModulesCard.jsx
+++ src/components/cards/favouriteModulesCard.jsx
@@ -2,20 +2,22 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { CONSTANTS } from 'helpers/urlConstants.js'
 import { replacePlaceHolder } from 'helpers/urlHelper.js'
+import API from 'helpers/api.js'
 
 class FavouriteModuleCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      toggleFavourite: true,
+      favouriteStatus: true,
     }
-    this.handleFavouriteClick = this.handleFavouriteClick.bind(this)
   }
 
-  handleFavouriteClick() {
-    this.setState(state => ({
-      toggleFavourite: !state.toggleFavourite
-    }))
+  handleFavouriteClick = () => {
+    API.toggleFavouriteModule(this.props.data.program.id, this.props.data.module.id, this.stateHandler, this.state.favouriteStatus)
+  }
+  
+  stateHandler = (state) => {
+    this.setState(state);
   }
 
   render() {
@@ -26,7 +28,7 @@ class FavouriteModuleCard extends Component {
             <div className="row">
               <div className="col s10 m10 l10 card-title left-align"> Program {this.props.data.program.id}: {this.props.data.program.title} </div>
               <i className="col s1 m1 l1 material-icons btn-flat goal-icon"> directions_run </i>
-              <i className="col s1 m1 l1 material-icons btn-flat favourite-icon" id={"favourite-module_" + this.props.data.programId + "-favourite-icon"} style={{opacity: this.state.toggleFavourite ? '1.0' : '0.2'}} onClick={this.handleFavouriteClick}> favorite </i>
+              <i className="col s1 m1 l1 material-icons btn-flat favourite-icon" id={"favourite-module_" + this.props.data.programId + "-favourite-icon"} style={{ opacity: this.state.favouriteStatus ? '1.0' : '0.2'}} onClick={this.handleFavouriteClick}> favorite </i>
             </div>
           </div>
           <Link to={replacePlaceHolder(CONSTANTS.MODULES,[this.props.data.program.id, this.props.data.module.id])}>
